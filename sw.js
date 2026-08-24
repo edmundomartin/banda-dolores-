947a3b7c9e6fbb8e82811c99bd30e2a5411be7a9
edmundomartin <edmundomartinm@gmail.com>
2026-08-24 21:45:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='dolores-v04';
-const ASSETS=['./','index.html','manifest.webmanifest','icon.svg','icon-192.png','icon-512.png','portada.jpeg'];
+const CACHE='dolores-v05';
+const ASSETS=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','logo-original.png','portada.jpeg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
